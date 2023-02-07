d2d8679ee297da92a7984a6aefa5b0d248952050
Vitalii Smyrnov <113116677+Vetal-Smyrnov@users.noreply.github.com>
2023-02-07 15:02:26 +0300

    Delete src/js directory

--- src/js/task-07.js
+++
@@ -1,6 +0,0 @@
-const inputEl = document.querySelector('input#font-size-control');
-const textSpanEl = document.querySelector('#text');
-
-const inputHandler = () => textSpanEl.style.fontSize = `${inputEl.value}px`;
-
-inputEl.addEventListener('input', inputHandler);
